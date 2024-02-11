773c152950eba0503a33544e52eb9821315814bc
ZaynOssama <oussamazazou@iesmontsia.org>
2024-02-11 18:26:32 +0100

    All styles in one file

--- test-app/resources/views/layouts/app.blade.php
+++ test-app/resources/views/layouts/app.blade.php
@@ -10,33 +10,40 @@
     <link href="https://fonts.googleapis.com/css2?family=Protest+Guerrilla&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <!-- Styles -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.3/jspdf.umd.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
     <style>
-        html {
-            box-sizing: border-box;
-            font-size: 50%;
-            /* 10px = 1rem */
-        }
-
-        *,
-        *:before,
-        *:after {
-            box-sizing: inherit;
-        }
 
         body {
-            font-family: 'Kanit', sans-serif;
+            font-family: "Kanit", sans-serif;
             font-size: 1.8rem;
             line-height: 1;
-            background-image: url('{{ url('storage/pylon-principal.jpg') }}');
+            background-image: url("storage/pylon-principal.jpg");
+        }
+
+        @media (min-width: 768px) {
+            .front-page {
+                display: block;
+                width: 100%;
+                height: auto;
+                background-image: linear-gradient(-45deg,
+                        rgba(255, 255, 255, 0.682) 50%,
+                        transparent 50%),
+                    url(storage/images.jpg);
+                background-size: cover;
+                /* Esto asegurará que la imagen se ajuste al tamaño del contenedor */
+                background-position: center;
+                /* Esto asegurará que la imagen esté centrada */
+            }
         }
     </style>
-    @yield('styles')
     <title>Test App @yield('title')</title>
 </head>
 
 <body>
-    @yield('content')
+    <div id="content">
+        @yield('content')
+    </div>
 
     @yield('scripts')
 </body>
